b492eadf9be6e1d6baf2fa05eb670a93408cf0e6
Lucas <lucasgrafimar@gmail.com>
2023-03-03 16:16:28 -0300

    Adição do modulo Morgans para o log no backend e adição do cors para requisição no front.

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
 const routes = require("./routes");
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
+app.use(morgan("dev"));
 app.use(routes);
 
 app.use((req, res, next) => {
