0d800216eeebd9a9605cb060d11c1637878597eb
Lucas <lucasgrafimar@gmail.com>
2023-03-04 17:23:45 -0300

    Adição de uma verificação para retornar um erro caso o edificio ja exista

--- Backend/src/controllers/buildingController.js
+++ Backend/src/controllers/buildingController.js
@@ -8,8 +8,14 @@ module.exports = {
   async create(req, res, next) {
     const { name_building } = req.body;
     try {
-      await knex("buildings").insert({ name_building });
-      return res.status(201).send();
+      const results = await knex("buildings").where({ name_building });
+      if (results) {
+        await knex("buildings").insert({ name_building });
+        const id = await knex("buildings").where({ name_building });
+        return res.status(201).send({ id: id[0].id });
+      } else {
+        return res.status(404).send("Ja existe esse predio.");
+      }
     } catch (error) {
       next(error);
     }
